cd9667fea7144341161630a8a953f46755ac8421
nitinthite16 <nitinthite030@gmail.com>
2024-04-12 17:04:40 +0530

    Your commit message here

--- src/main/java/com/ElectronicStoreAppApplication.java
+++ src/main/java/com/ElectronicStoreAppApplication.java
@@ -8,6 +8,7 @@ public class ElectronicStoreAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicStoreAppApplication.class, args);
+		System.out.println("Application started........!!!");
 	}
 
 }
